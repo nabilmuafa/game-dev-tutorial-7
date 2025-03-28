aa99a006bebca84d4c165d259521d281e039ec7e
nabilmuafa <nmuafa@gmail.com>
2025-03-28 14:32:48 +0700

    finish tutorial

---
+++ scenes/goal_condition.gd
@@ -0,0 +1,10 @@
+extends Area3D
+
+@export var sceneName := "Level 1"
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.get_name() == "Player":
+		if sceneName == "Level 1":
+			$"../AudioStreamPlayer3D".play()
+			await $"../AudioStreamPlayer3D".finished  # Wait for sound to finish before transitioning
+		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
