13a2477d8db56c98e455be2a5f9b6bdc4a9c1edf
nabilmuafa <nmuafa@gmail.com>
2025-03-28 15:22:14 +0700

    finish latihan mandiri

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,15 +1,27 @@
 extends CharacterBody3D
 
-@export var speed: float = 10.0
-@export var acceleration: float = 5.0
+@export var speed: float
+@export var acceleration: float = 3.0
 @export var gravity: float = 9.8
-@export var jump_power: float = 5.0
+@export var jump_power: float = 4.5
 @export var mouse_sensitivity: float = 0.3
+@export var crouch_height: float = -0.5
+@export var standing_height: float = 0.0
 
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
 
 var camera_x_rotation: float = 0.0
+var target_camera_height: float = 0.0
+
+const CROUCH_SPEED = 4.0
+const WALK_SPEED = 8.0
+const SPRINT_SPEED = 16.0
+
+#head bob
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -25,28 +37,48 @@ func _input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _physics_process(delta):
-	var movement_vector = Vector3.ZERO
-
-	if Input.is_action_pressed("movement_forward"):
-		movement_vector -= head.basis.z
-	if Input.is_action_pressed("movement_backward"):
-		movement_vector += head.basis.z
-	if Input.is_action_pressed("movement_left"):
-		movement_vector -= head.basis.x
-	if Input.is_action_pressed("movement_right"):
-		movement_vector += head.basis.x
-
-	movement_vector = movement_vector.normalized()
-
-	velocity.x = lerp(velocity.x, movement_vector.x * speed, acceleration * delta)
-	velocity.z = lerp(velocity.z, movement_vector.z * speed, acceleration * delta)
-
 	# Apply gravity
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+		
 	# Jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_power
+		
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+		target_camera_height = standing_height
+	elif Input.is_action_pressed("crouch"):
+		speed = CROUCH_SPEED
+		target_camera_height = crouch_height
+	else:
+		speed = WALK_SPEED
+		target_camera_height = standing_height
+		
+	var input_dir = Input.get_vector("movement_left", "movement_right", "movement_forward", "movement_backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * acceleration)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * acceleration)
+		
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	var bob_offset = _headbob(t_bob)
+	camera.transform.origin = bob_offset
 
 	move_and_slide()
+	
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	if target_camera_height == crouch_height:
+		pos.y -= 0.5
+	return pos
+	
--- scenes/goal_condition.gd
+++ scenes/goal_condition.gd
@@ -7,4 +7,7 @@ func _on_body_entered(body: Node3D) -> void:
 		if sceneName == "Level 1":
 			$"../AudioStreamPlayer3D".play()
 			await $"../AudioStreamPlayer3D".finished  # Wait for sound to finish before transitioning
-		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
+		call_deferred("change_scene")  # Defer the scene change
+
+func change_scene():
+	get_tree().change_scene_to_file("res://scenes/" + sceneName + ".tscn")
